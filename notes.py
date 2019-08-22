f88093c54244cf5867af526de2e0d31522716d77
Gavin Purcell <mkorangestripe@gmail.com>
2019-08-22 14:28:06 -0500

    Remove decorators example.

--- notes.py
+++ notes.py
@@ -556,36 +556,3 @@ def greet_me(**kwargs):
 
 greet_me(name="yasoob")
 # name = yasoob
-
-
-
-# Decorators:
-# They basically act as wrappers.
-
-def a_new_decorator(a_func):
-    def wrapTheFunction():
-        print("I am doing some boring work before executing a_func()")
-        a_func()
-        print("I am doing some boring work after executing a_func()")
-    return wrapTheFunction
-
-def a_function_requiring_decoration():
-   print("I am the function which needs some decoration to remove my foul smell")
-
-a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
-
-a_function_requiring_decoration()
-# I am doing some boring work before executing a_func()
-# I am the function which needs some decoration to remove my foul smell
-# I am doing some boring work after executing a_func()
-
-# The same as above but using a decorator.
-# Now when a_function_requiring_decoration() is called, it's passed to a_new_decorator().
-
-@a_new_decorator
-def a_function_requiring_decoration():
-    """Hey you! Decorate me!"""
-    print("I am the function which needs some decoration to "
-          "remove my foul smell")
-
-a_function_requiring_decoration()
