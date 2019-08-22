2e0419bc37a5d4b83844147937fb125b6c2688aa
Gavin Purcell <mkorangestripe@gmail.com>
2019-08-22 16:32:01 -0500

    Move map, filter, and reduce to map_filter_reduce.py

--- notes.py
+++ notes.py
@@ -197,43 +197,6 @@ print selfref_list[3][1]
 
 
 
-# Filter
-# Takes a function and a list, tuple, or string as input, returns list of True items.
-# Returns numbers divisible by 2 or 3
-def f(x):
-    return x % 2 != 0 and x % 3 != 0
-filter(f, range(2, 25))
-# [5, 7, 11, 13, 17, 19, 23]
-
-# Map
-# Takes a function and a list(s) or tuple(s) as input, returns list of results.
-# Returns cubes of the numbers in the range
-def cube(x):
-    return x*x*x
-map(cube, range(1, 11))
- # [1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]
-
-# Returns sums of corresponding elements
-seq = range(8)
-def add(x, y):
-    return x+y
-map(add, seq, seq)
- # [0, 2, 4, 6, 8, 10, 12, 14]
-
-# Reduce
-# Takes a function (with two args) and lists or tuples as input and applies
-# the function cumulatively to the items of the sequence, from left to right,
-# so as to reduce the sequence to a single value.
-
-# Calls the function 'add' with the first two items in the sequence,
-# then on the result and the next item, and so on.
-def add(x,y):
-    return x+y
-reduce(add, range(1, 11))
-# 55
-
-
-
 # Print index and values of list
 cassandra_endpoints=['10.141.18.229', '10.141.18.214', '10.141.18.219']
 
