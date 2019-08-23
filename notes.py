09e9c680c62d1ed69069acd823ee4e337d2efe0a
Gavin Purcell <mkorangestripe@gmail.com>
2019-08-23 14:38:56 -0500

    Move type conversion to type_conversion.py

--- notes.py
+++ notes.py
@@ -1,25 +1,14 @@
 # Sets the python path before running the shell:
 # PYTHONPATH=./src ipython
 
+# Return a list of valid attributes for the object
+dir(list)
 
-# Convert between chr and int (integer ordinal):
-ord('A') # 65
-chr(65) # A
-# Convert between hex and int
-int(0x41) # 65
-hex(65) # 0x41
-# Convert between bin and int
-int(0b1000001) # 65
-bin(65) # 0b1000001
-# Convert between oct and int
-int(0101) # 65
-oct(65) # 0101
+# Help on item
+help(list)
 
 # Multiple assignment:
 a, b = 9, 2
-
-# Return a list of valid attributes for the object:
-dir(list)
 
 # Use a global variable in a function:
 global num
---
+++ type_conversion.py
@@ -0,0 +1,12 @@
+# Convert between chr and int (integer ordinal):
+ord('A') # 65
+chr(65) # A
+# Convert between hex and int
+int(0x41) # 65
+hex(65) # 0x41
+# Convert between bin and int
+int(0b1000001) # 65
+bin(65) # 0b1000001
+# Convert between oct and int
+int(0101) # 65
+oct(65) # 0101
