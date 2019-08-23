662aad5cdeb49d0b3199b1855ce25b3b72aa62f7
Gavin Purcell <mkorangestripe@gmail.com>
2019-08-23 16:07:37 -0500

    Add breakpoint debugging

--- error_handling.py
+++ error_handling.py
@@ -23,3 +23,46 @@ for arg in sys.argv[1:]:
 
 # Raise a runtime error:
 raise RuntimeError('test for validate_request_parameters debug log')
+
+
+# Breakpoint Debugging with PDB
+# example from linuxacademy.com
+import pdb
+
+def map(func, values):
+    output_values = []
+    index = 0
+    while index < len(values):
+        pdb.set_trace()
+        output_values = func(values[index])
+        index += 1
+    return output_values
+
+def add_one(val):
+    return val + 1
+
+print(map(add_one, list(range(10))))
+
+# Run above as follows...
+# python3.7 debugging.py
+# pdb will break at the pbd.set_trace line and will display a (Pdb) prompt.
+values
+# [0, 1, 2, 3, 4]
+index
+# 0
+output_values
+# []
+n
+# index += 1
+
+# Or run the file without the import line.
+# This will step through line by line from the top.
+# python3.7 -m pdb debugging.py
+# ll
+# Set a breakpoint at line 5 when index is 5
+break 5, index == 5
+# c
+
+# h for help
+# 'help cont' for help on cont
+# q for quit
