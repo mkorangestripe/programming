9b8103b2218ec724739d510f4deec3eae21201f8
Gavin Purcell <mkorangestripe@gmail.com>
2019-11-01 13:21:02 -0500

    Rename file and add a comment.

--- data_graphing.py
+++ data_graphing.py
@@ -35,18 +35,22 @@ plt.plot(x,y,'ro')
 plt.show()
 
 
-# Scaling data, to fit between 0 and 1.
+# Scaling data to fit between 0 and 1.
 
-#   Number of kinds of teeth
-#   Wolf                33114423
-#   Brown Bat           23113333
-#   Rat                 22000066
-#   Human               22112233
+# Number of kinds of teeth
+# Wolf                33114423
+# Brown Bat           23113333
+# Rat                 22000066
+# Human               22112233
 
+Wolf = ['3', '3', '1', '1', '4', '4', '2', '3']
+Rat = ['2', '2', '0', '0', '0', '0', '6', '6']
+
+# Max is form the animal with the most of a tooth type.
 scaleDict = {'identity': [1,1,1,1,1,1,1,1],
              '1/max': [1/3.0,1/4.0,1.0,1.0,1/4.0,1/4.0,1/6.0,1/6.0]}
 
-# For each tooth, multiple the tooth by max, e.g.
+# For each tooth, multiply the tooth by max, e.g.
 for i in range(len(Wolf)):
     print float(Wolf[i]) *  scaleDict['1/max'][i]
 
