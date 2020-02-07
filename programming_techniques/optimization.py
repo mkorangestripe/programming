de89ddd9443ae0b061373e56c9561f9fb238ad26
Gavin Purcell <mkorangestripe@gmail.com>
2020-02-07 11:22:56 -0600

    Move fibonacci example to optimization.py

--- programming_techniques/optimization.py
+++ programming_techniques/optimization.py
@@ -350,3 +350,26 @@ for numItems in (5, 10, 15, 20, 25, 30, 35, 40, 45, 50):
 #     5: <81, 7>
 #     2: <75, 37>
 #     1: <66, 20>
+
+
+
+# Fibonacci sequence using memoization
+# By definition, the first two numbers in the Fibonacci sequence are 1 and 1, or 0 and 1...
+# depending on the chosen starting point of the sequence, and each subsequent number is the sum of the previous two.
+# By using the memo, the order of growth drops from 2^n to n^2 ???
+
+memo0 = {0:0, 1:1} ## 0, 1, 1, 2, 3, 5, 8, 13, 21
+# memo1 = {0:1, 1:1} ## 1, 1, 2, 3, 5, 8, 13, 21
+def fibonacci(n):
+    if not n in memo0:
+        memo0[n] = fibonacci(n-1) + fibonacci(n-2)
+    return memo0[n]
+
+
+# The Golden Ratio
+# Line segment of segments 'a' and 'b' where a+b is to 'a' as 'a' is to 'b'.
+# The ration is about 1.618.  The higher the numbers in the Fibonacci sqequence, the closer their ratio to the Golden ratio.
+# The Greek letter φ (phi) is used to symbolize the golden ratio.
+# If a Fibonacci number is divided by its immediate predecessor in the sequence, the quotient approximates φ.
+# These approximations are alternately lower and higher than φ, and converge on φ as the Fibonacci numbers increase.
+# The golden spiral is a logarithmic spiral whose growth factor is φ, the golden ratio.
