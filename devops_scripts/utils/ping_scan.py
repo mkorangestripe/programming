be94c5bdd53c035831e6740a68d0f923c714d195
Gavin Purcell <mkorangestripe@gmail.com>
2020-05-19 14:29:14 -0500

    Add ipaddress module to create cidr ranges, start adding argparse.

--- devops_scripts/utils/ping_scan.py
+++ devops_scripts/utils/ping_scan.py
@@ -1,9 +1,44 @@
-#!/usr/bin/env python
-# Ping in Parallel
-import subprocess
+#!/usr/bin/env python3
+"""
+Scan a network by pinging a CIDR range in parallel.'
+"""
 
-IPs = ['192.168.0.1', '192.168.0.2', '192.168.0.3', '192.168.0.14', '192.168.0.15', '192.168.0.16']
+# To do:
+# default mode: up/down
+# verbose: output specific failure message per host
+# use argparse args
+# add -c count to args
+
+import subprocess
+import ipaddress
+from argparse import ArgumentParser
+
+class color:
+    green = '\033[92m'
+    end = '\033[0m'
+
+# For testing...
+# IPs = ['192.168.0.1', '192.168.0.2', '192.168.0.3', '192.168.0.14', '192.168.0.15', '192.168.0.16']
+# net_addr = '192.168.0.0/28'
+net_addr = '192.168.0.0/27'
+# ip_net = ipaddress.ip_network(net_addr, strict=False)
+ip_net = ipaddress.ip_network(net_addr)
+all_hosts = list(ip_net.hosts())
+IPs = all_hosts
 deadline = 5  # specified by ping -w
+
+
+def _get_argparser():
+    parser = ArgumentParser(description=__doc__)
+    # parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("--infile",
+                        help="input file. This expects subnets in CIDR notation.")
+    parser.add_argument("--outfile",
+                        help="output file. This will out put the result of a scan. default: outfile.txt", default="outfile.txt")
+    parser.add_argument("--cidr",
+                        help="Scan a CIDR range i.e. 10.0.0.0/24 which would scan address range 10.0.0.0 - 10.0.0.255")
+    parser.add_argument('-w', '--deadline', help='Specify a timeout, in seconds, before ping exits')
+    args = parser.parse_args()
 
 
 def run_ping_cmd(ipaddr, ping_deadline):
@@ -17,13 +52,15 @@ def run_ping_cmd(ipaddr, ping_deadline):
 jobs = []
 ping_response_by_IP = {}
 for IP in IPs:
+    IP = str(IP)
     ping_response_by_IP[IP] = []
     process = run_ping_cmd(IP, deadline)
     jobs.append((process, IP))
 
+print('Starting ping of', net_addr)
 for job in jobs:
     process, IP = job[0], job[1]
-    print("Waiting for " + IP)
+    # print("Waiting for " + IP)
     process.wait()
     return_code = process.returncode
     ping_response = process.stdout.readlines()
@@ -31,12 +68,13 @@ for job in jobs:
 print('')
 
 for IP in IPs:
+    IP = str(IP)
     return_code = ping_response_by_IP[IP][0]
     if return_code == 0:
-        print(IP + ' is up')
+        # print(IP + ' is up')
+        print('%s is %sup%s' % (IP, color.green, color.end))
     else:
         print(IP + ' is down')
-print('')
 
 # for IP in IPs:
 #     stdout = ping_response_by_IP[IP][1]
