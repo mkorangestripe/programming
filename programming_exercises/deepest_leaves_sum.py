89ac86f47f497fd604ddc29817961f5d62dec1eb
Gavin Purcell <mkorangestripe@gmail.com>
2020-05-28 10:58:14 -0500

    Add deepest_leaves_sum.py

---
+++ programming_exercises/deepest_leaves_sum.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# Given a binary tree, return the sum of values of its deepest leaves.
+
+
+# Definition for the binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+# root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+btree = TreeNode(1)
+btree.left = TreeNode(2)
+btree.right = TreeNode(3)
+btree.left.left = TreeNode(4)
+btree.left.right = TreeNode(5)
+btree.right.right = TreeNode(6)
+btree.left.left.left = TreeNode(7)
+btree.right.right.right = TreeNode(8)
+
+
+class Solution:
+
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        depth = 0
+        total = 0
+
+        def sumTree(root, curdepth):
+            nonlocal depth
+            nonlocal total
+
+            if root is None:
+                return
+
+            if curdepth > depth:
+                depth = curdepth
+                total = root.val
+            elif curdepth == depth:
+                total += root.val
+
+            sumTree(root.left, curdepth + 1)
+            sumTree(root.right, curdepth + 1)
+
+        sumTree(root, depth)
+        return total
+
+
+solution = Solution()
+deepest_leaves_sum = solution.deepestLeavesSum(btree)
+print(deepest_leaves_sum)  # 15
