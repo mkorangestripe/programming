e5c01a8df4918f767775416b53151442fa578d3d
Gavin Purcell <mkorangestripe@gmail.com>
2020-06-18 15:24:32 -0500

    Add install_requires to setup.py

--- python_various_examples/pip_virtualenv_tox/ping_scan_mkorangestripe/setup.py
+++ python_various_examples/pip_virtualenv_tox/ping_scan_mkorangestripe/setup.py
@@ -18,5 +18,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        "sys",
+        "subprocess",
+        "ipaddress",
+        "argparse"
+    ],
     python_requires='>=3.6',
 )
