b8b40056f3ec925f0de4eaf144c288bdad457371
Gavin Purcell <gavin.purcell@protolabs.com>
2021-02-20 22:04:54 -0600

    Move notes and examples elsewhere

--- notes_and_examples/misc.py
+++
@@ -1,8 +0,0 @@
-# Sets the python path before running the shell:
-# PYTHONPATH=./src ipython
-
-# Return a list of valid attributes for the object
-dir(list)
-
-# Help on item
-help(list)
--- notes_and_examples/pip_virtualenv_tox/ping_scan_mkorangestripe/README.md
+++
@@ -1,5 +0,0 @@
-# ping_scan.py
-
-### ping_scan.py
-###### Scan a network by pinging a CIDR range in parallel.
-![Output from ping_scan.py](sample-output/ping_scan.png)
