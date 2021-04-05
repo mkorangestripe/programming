fb59e2230b0c3cb2310115745cab9b6e9ed0d7c1
Gavin Purcell <mkorangestripe@gmail.com>
2021-04-04 22:28:10 -0500

    Update variables mostly

--- exercises/VMscheduler.py
+++ exercises/VMscheduler.py
@@ -6,21 +6,27 @@ schedule2 = [(2,5),(3,6),(5,7),(5,8)]
 schedule3 = [(2,5),(3,5),(5,7)]
 schedule4 = []
 
-
 def vm_schedule_count(schedule):
-    i,j = 0,0
-    for times in schedule:
-        start = times[0]
-        for prev_times in schedule[i:j]:
-            prev_end = prev_times[1]
-            if prev_end <= start:
-                i += 1
+    """The schedule contains reservations with start and end times."""
+    trailing_res,leading_res = 0,0
+    start,end = 0,1
+
+    for reservation in schedule:
+        for prev_reservation in schedule[trailing_res:leading_res]:
+            if prev_reservation[end] <= reservation[start]:
+                trailing_res += 1
                 break
-        j += 1
-    return len(schedule[i:j])
+        leading_res += 1
+
+    return len(schedule[trailing_res:leading_res])
 
 
-vm_schedule_count(schedule1)  # 2
-vm_schedule_count(schedule2)  # 3
-vm_schedule_count(schedule3)  # 2
-vm_schedule_count(schedule4)  # 0
+schedules = [schedule1, schedule2, schedule3, schedule4]
+for schedule in schedules:
+    vm_count = vm_schedule_count(schedule)
+    print(schedule, str(vm_count))
+
+# schedule1  # 2
+# schedule2  # 3
+# schedule3  # 2
+# schedule4  # 0
