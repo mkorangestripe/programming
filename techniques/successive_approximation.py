ef7359702ba51f7e866d8ac8b7e660ce72320848
Gavin Purcell <mkorangestripe@gmail.com>
2024-08-12 10:42:01 -0500

    Update successive_approximation.py

--- techniques/successive_approximation.py
+++ techniques/successive_approximation.py
@@ -9,52 +9,60 @@ than without using the bisection method.
 Examples from MIT OCW
 """
 
-# Simple guess-and-check
-X = 2500
-NUM_GUESSES = 0
+print("Simple guess-and-check to find square root")
+X = 2500  # Number of guesses: 51
+print("X:", X)
+num_guesses = 0
 UPPER = X//2
 for i in range(0, UPPER):
-    NUM_GUESSES += 1
+    num_guesses += 1
     if i**2 == X:
-        print('NUM_GUESSES =', NUM_GUESSES)
+        print('Number of guesses:', num_guesses)
         break
 
-# NUM_GUESSES = 51
+print()
+print("Successive approximation using guess-and-check and bisection methods")
+X = 25  # Number of guesses: 13
+# X = 2500  # Number of guesses: 20
+EPSILON = 0.01  # tolerance of the answer^2 to X
 
-
-# Successive approximation using guess-and-check and bisection methods.
-# For 'X = 2500', NUM_GUESSES = 20
-X = 25
-EPSILON = 0.01
-NUM_GUESSES = 0
-LOW = 0.0
+num_guesses = 0
+low = 0.0
 high = max(X, 1)
-ans = (high + LOW)/2.0
+ans = (high + low)/2.0
 
+print("X:", X)
+print("Tolerance:", EPSILON)
+
+print()
+print("LOW, ANSWER, HIGH")
 while abs(ans**2 - X) >= EPSILON and ans <= X:
-    print(LOW, high, ans)
-    NUM_GUESSES += 1
+    print(low, ans, high)
+    num_guesses += 1
     if ans**2 < X:
-        LOW = ans
+        low = ans
     else:
         high = ans
-    ans = (high + LOW)/2.0
+    ans = (high + low)/2.0
 
-print('NUM_GUESSES =', NUM_GUESSES)
-print(ans, 'is close to square root of', X)
+print()
+print(ans, 'is close enough to square root of', X)
+print('Number of guesses:', num_guesses)
 
-# 0.0 25 12.5
-# 0.0 12.5 6.25
-# 0.0 6.25 3.125
-# 3.125 6.25 4.6875
-# 4.6875 6.25 5.46875
-# 4.6875 5.46875 5.078125
-# 4.6875 5.078125 4.8828125
-# 4.8828125 5.078125 4.98046875
-# 4.98046875 5.078125 5.029296875
-# 4.98046875 5.029296875 5.0048828125
-# 4.98046875 5.0048828125 4.99267578125
-# 4.99267578125 5.0048828125 4.99877929688
-# 4.99877929688 5.0048828125 5.00183105469
-# NUM_GUESSES = 13
-# 5.00030517578 is close to square root of 25
+# LOW, ANSWER, HIGH
+# 0.0 12.5 25
+# 0.0 6.25 12.5
+# 0.0 3.125 6.25
+# 3.125 4.6875 6.25
+# 4.6875 5.46875 6.25
+# 4.6875 5.078125 5.46875
+# 4.6875 4.8828125 5.078125
+# 4.8828125 4.98046875 5.078125
+# 4.98046875 5.029296875 5.078125
+# 4.98046875 5.0048828125 5.029296875
+# 4.98046875 4.99267578125 5.0048828125
+# 4.99267578125 4.998779296875 5.0048828125
+# 4.998779296875 5.0018310546875 5.0048828125
+
+# 5.00030517578125 is close enough to square root of 25
+# Number of guesses: 13
